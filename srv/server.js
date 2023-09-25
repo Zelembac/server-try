9c8a5908a097777b29af4a36440abe71a93a2ccc
Lazar Djurkovic <92604467+Zelembac@users.noreply.github.com>
2023-09-25 12:31:06 +0200

    Update server.js

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
-let jsonData = JSON.parse(fs.readFileSync("setup.json"));
-console.log(jsonData);
+// let jsonData = JSON.parse(fs.readFileSync("setup.json"));
+// console.log(jsonData);
 
 const express = require("express");
 const path = require("path");
@@ -14,17 +14,17 @@ app.use(express.static(path.join(__dirname)));
 const io = require("socket.io")(server);
 const users = {};
 
-io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("promeni", (text) => {
-    let jsonData1 = JSON.parse(fs.readFileSync("setup.json"));
-    jsonData1.backgroundColor = text;
+// io.on("connection", (socket) => {
+//   console.log(socket.id);
+//   socket.on("promeni", (text) => {
+//     let jsonData1 = JSON.parse(fs.readFileSync("setup.json"));
+//     jsonData1.backgroundColor = text;
 
-    fs.writeFileSync("setup.json", JSON.stringify(jsonData1));
+//     fs.writeFileSync("setup.json", JSON.stringify(jsonData1));
 
-    console.log(jsonData1.backgroundColor);
-    socket.emit("promenjena", "boja je promenjena");
-  });
-});
+//     console.log(jsonData1.backgroundColor);
+//     socket.emit("promenjena", "boja je promenjena");
+//   });
+// });
 
 server.listen(process.env.PORT || 5000);
