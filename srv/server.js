f21b90b7f9df24a57de8e8a04d79c9a02d8d66a0
Lazar Djurkovic <92604467+Zelembac@users.noreply.github.com>
2023-09-25 13:58:20 +0200

    Update server.js

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,7 @@
 var fs = require("fs");
 
-// let jsonData = JSON.parse(fs.readFileSync("setup.json"));
-// console.log(jsonData);
+let jsonData = JSON.parse(fs.readFileSync("https://github.com/Zelembac/server-try/blob/main/setup.json"));
+console.log(jsonData);
 
 const express = require("express");
 const path = require("path");
@@ -11,20 +11,20 @@ const server = require("http").createServer(app);
 
 app.use(express.static(path.join(__dirname)));
 
-const io = require("socket.io")(server);
+const io = require("https://github.com/Zelembac/server-try/blob/main/setup.json")(server);
 const users = {};
 
-// io.on("connection", (socket) => {
-//   console.log(socket.id);
-//   socket.on("promeni", (text) => {
-//     let jsonData1 = JSON.parse(fs.readFileSync("setup.json"));
-//     jsonData1.backgroundColor = text;
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("promeni", (text) => {
+    let jsonData1 = JSON.parse(fs.readFileSync("setup.json"));
+    jsonData1.backgroundColor = text;
 
-//     fs.writeFileSync("setup.json", JSON.stringify(jsonData1));
+    fs.writeFileSync("https://github.com/Zelembac/server-try/blob/main/setup.json", JSON.stringify(jsonData1));
 
-//     console.log(jsonData1.backgroundColor);
-//     socket.emit("promenjena", "boja je promenjena");
-//   });
-// });
+    console.log(jsonData1.backgroundColor);
+    socket.emit("promenjena", "boja je promenjena");
+  });
+});
 
 server.listen(process.env.PORT || 5000,()=>console.log('server is running on port 5000'));
